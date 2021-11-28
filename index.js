efc83f8ff62b3aa8848aa133c37c2b4815fae6ad
Gui Prá <gui@supersaas.com>
2021-11-29 00:29:33 +0100

    Editor setup

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import App from './components/App.jsx';
 import d from '@dominant/core';
 
-if (process.env.NODE_ENV === 'development') { window.d = d }
-
+window.d = d;
 document.querySelector('#app-wrapper').append(<App />);
